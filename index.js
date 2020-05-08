b01d2db14c39943cc82525662ecdf6035f325013
Thinh Tran <thinhtd2@vng.com.vn>
2020-05-08 12:46:20 +0700

    feat: add question c

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const knightInput = "152 1 0 1 1";
-const journeyInput = "3 5";
+const knightInput = "998 1 0 0 0";
+const journeyInput = "4 6 1 1 1";
 
 const baseDamage = {
     1: 1,
@@ -13,6 +13,8 @@ let result;
 let hp, level, remedy, maidenKiss, phoenixDown, maxhp;
 let knightArr = [];
 let journeyArr = [];
+let isTiny = false;
+let tinyRound = 3;
 
 const preprocess = () => {
     knightArr = knightInput.split(" ").map((i) => Number(i));
@@ -28,21 +30,53 @@ const calcResult = () => {
 const getDamage = (event, levelO) => {
     return baseDamage[event] * 10 * levelO;
 };
-const handleEventFrom1to5 = (i, event) => {
+const handleEventFrom1to6 = (i, event) => {
     const index = i + 1;
     const b = index % 10;
     const levelO = index > 6 ? (b > 5 ? b : 5) : b;
+    console.log(" hp, level, remedy, maidenKiss, phoenixDown, maxhp", [
+        hp,
+        level,
+        remedy,
+        maidenKiss,
+        phoenixDown,
+        maxhp,
+    ]);
 
-    if (level === levelO) return;
-    if (level > levelO) {
-        level = level + 1 > 10 ? 10 : level + 1;
-    } else {
-        hp = parseInt(hp - getDamage(event, levelO));
-        if (hp < 1) {
-            if (phoenixDown < 1) return -1;
-            hp = maxhp;
-            phoenixDown = phoenixDown - 1;
+    const fightMonster = () => {
+        if (level === levelO) return;
+        if (level > levelO) {
+            level = level + 1 > 10 ? 10 : level + 1;
+        } else {
+            hp = parseInt(hp - getDamage(event, levelO));
+            if (hp < 1) {
+                if (phoenixDown < 1) return -1;
+                hp = maxhp;
+                phoenixDown = phoenixDown - 1;
+                isTiny = false;
+            }
         }
+    };
+    const fightShaman = () => {
+        if (level === levelO) return;
+        if (level > levelO) {
+            level = level + 2 > 10 ? 10 : level + 2;
+        } else {
+            hp = hp < 5 ? 1 : parseInt(hp / 5);
+            if (remedy < 1) {
+                isTiny = true;
+                tinyRound = 3;
+            } else {
+                remedy = remedy - 1;
+                hp = hp * 5;
+            }
+        }
+    };
+
+    if (event < 6) {
+        fightMonster();
+    } else {
+        fightShaman();
     }
 };
 
@@ -52,8 +86,17 @@ const calculateJouney = () => {
         if (event === 0) {
             return;
         }
-        if (event < 6) {
-            handleEventFrom1to5(i, event);
+        if (event < 7) {
+            handleEventFrom1to6(i, event);
+        }
+        // console.log("tinyRound", tinyRound);
+        if (isTiny) {
+            if (tinyRound === 0) {
+                hp = hp * 5;
+                isTiny = false;
+            }
+            tinyRound = tinyRound - 1;
+            console.log("tinyRound", tinyRound);
         }
     }
 };
